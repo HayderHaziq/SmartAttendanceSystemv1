76e2795c1df7909f686059763a6fdf12d8db632a
aeaver <hayderhaziq.shahrizal@student.gmi.edu.my>
2023-11-27 07:34:40 +0800

    aaaa

--- app/Http/Controllers/AttendanceApiController.php
+++ app/Http/Controllers/AttendanceApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Attendance;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AttendanceApiController extends Controller
 {
@@ -18,36 +19,46 @@ class AttendanceApiController extends Controller
         $validatedData = $request->validate([
             'ndef_data' => 'required|string',
         ]);
-
-        // Process the ndef data
-        $ndefParts = explode(',', $validatedData['ndef_data']);
-
-        if (count($ndefParts) == 2) {
-            $studentId = $ndefParts[0];
-            $eventId = $ndefParts[1];
-
-            $attendance = Attendance::where('student_id', $studentId)
-                ->where('date', now()->toDateString())
-                ->first();
-
-            if ($attendance) {
-                $attendance->status = 'Present';
-                $attendance->time = now()->toTimeString(); // Update the time
-                $attendance->save();
-
-                return response()->json(['message' => 'Attendance updated successfully']);
+    
+        try {
+            DB::beginTransaction();
+    
+            // Process the ndef data
+            $ndefParts = explode(',', $validatedData['ndef_data']);
+    
+            if (count($ndefParts) == 2) {
+                $studentId = $ndefParts[0];
+                $classId = $ndefParts[1];
+    
+                // Check if both user ID and class ID exist in the database
+                $userExists = Student::where('id', $studentId)->exists();
+                $classExists = ClassModel::where('id', $classId)->exists();
+    
+                if ($userExists && $classExists) {
+                    // Both user and class exist, proceed with creating a new attendance record
+    
+                    Attendance::create([
+                        'student_id' => $studentId,
+                        'time' => now()->toTimeString(),
+                        'date' => now()->toDateString(),
+                        'status' => 'Present',
+                        'class_id' => $classId,
+                    ]);
+    
+                    DB::commit();
+    
+                    return response()->json(['message' => 'Attendance marked successfully']);
+                } else {
+                    // Either user or class does not exist, reject the attendance record
+                    return response()->json(['error' => 'Invalid user ID or class ID'], 400);
+                }
             } else {
-                Attendance::create([
-                    'student_id' => $studentId,
-                    'time' => now()->toTimeString(),
-                    'date' => now()->toDateString(),
-                    'status' => 'Present',
-                ]);
-
-                return response()->json(['message' => 'Attendance marked successfully']);
+                return response()->json(['error' => 'Invalid NDEF data format'], 400);
             }
-        } else {
-            return response()->json(['error' => 'Invalid NDEF data format'], 400);
+        } catch (\Exception $e) {
+            DB::rollBack();
+    
+            return response()->json(['error' => 'An error occurred during the transaction'], 500);
         }
     }
-}
+}    
